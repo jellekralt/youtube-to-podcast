1dc36e08f2b7b49039352a1258993bc18d27e205
Jelle Kralt <jelle@jellekralt.nl>
2017-05-08 11:08:25 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express')
+const app = express()
+const Stream = require('stream');
+
+const xml2js = require('xml2js')
+const parseString = xml2js.parseString;
+
+const ffmpeg = require('fluent-ffmpeg');
+const command = ffmpeg();
+
+const ytdl = require('ytdl-core');
+
+const generateRSS = require('./src/podcast-rss');
+
+app.get('/audio/:id/podcast.mp3', function (req, res) {
+    let audio = ytdl('https://www.youtube.com/watch?v=' + req.params.id, { filter: 'audioonly' });
+
+    ffmpeg(audio)
+        .audioCodec('libmp3lame')
+        .audioBitrate(128)
+        .format('mp3')
+        .on('error', (err) => console.error(err))
+        .on('end', () => console.log('Finished!'))
+        .pipe(res, {
+            end: true
+        });
+});
+
+app.get('/podcast/:id/feed.rss', function(req, feedRes) {
+    let playlistUrl = `https://www.youtube.com/feeds/videos.xml?playlist_id=${req.params.id}`;
+    let host = req.protocol + '://' + req.get('host');
+    let fullUrl = host + req.originalUrl;
+
+    https.get(playlistUrl, function(res) {
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            try {
+                const parsedData = parsePaylistXML(rawData, function(err, data) {
+                    
+                    feedRes.set({ 'content-type': 'application/xml; charset=utf-8' })
+                    feedRes.send(generateRSS(data, fullUrl, host));
+                    feedRes.end();
+
+                });
+            } catch (e) {
+                console.error(e.message);
+                feedRes.end();
+            }
+        });
+        
+    });
+    
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
+});
+
+function parsePaylistXML(xml, cb) {
+    parseString(xml, function (err, data) {
+        cb(err, data);
+    });
+}
---
+++ src/podcast-rss.js
@@ -0,0 +1,64 @@
+const RSS = require('rss');
+
+module.exports = function generateRSS(data, feedUrl, host) {
+
+    let feed = data.feed;
+    let author = feed.author[0].name[0];
+
+    let podcast = new RSS({
+        title: data.feed.title[0],
+        description: `YouTube Playlist: ${data.feed.title[0]}`,
+        feed_url: feedUrl,
+        categories: ['YouTube'],
+        pubDate: new Date(feed.published[0]),
+        ttl: '60',
+        custom_namespaces: {
+           'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'
+        },
+        custom_elements: [
+            {'itunes:subtitle': 'YouTube Playlist'},
+            {'itunes:author': author},
+            {'itunes:summary': `YouTube Playlist: ${data.feed.title[0]}`},
+            {'itunes:owner': [
+                {'itunes:name': author}
+            ]},
+            {'itunes:image': {
+                _attr: {
+                    href: 'https://www.youtube.com/yt/brand/media/image/YouTube-icon-full_color.png'
+                }
+            }},
+            {'itunes:category': [
+                {_attr: {
+                    text: 'YouTube'
+                }}
+            ]}
+        ]
+    });
+
+    feed.entry.forEach((entry) => {
+        var mediaGroup = entry['media:group'][0];
+        var id = entry['yt:videoId'][0];
+        
+        podcast.item({
+            title: entry.title[0],
+            description: mediaGroup['media:description'][0],
+            url: `${host}/audio/${id}/podcast.mp3`,
+            guid: id,
+            date: 'May 27, 2012', // any format that js Date can parse.
+
+            custom_elements: [
+                {'itunes:author': entry.author[0].name[0]},
+                {'itunes:summary': mediaGroup['media:description'][0]},
+                {'itunes:image': {
+                    _attr: {
+                        href: mediaGroup['media:thumbnail'][0].$.url
+                    }
+                }}
+            ]
+        });
+    });
+    
+
+    // cache the xml to send to clients
+    return podcast.xml();
+}
